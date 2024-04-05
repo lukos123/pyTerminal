9f9ba00ffa5ad440482d73d418d01be2cac4b3b9
lukos123 <max.kabyshev@gmail.com>
2024-04-05 21:56:02 +0200

    os.name2

--- get_stdout.py
+++ get_stdout.py
@@ -3,11 +3,10 @@ import os
 
 
 def branch():
-    command = ""
+    command = "/usr/bin/git branch"
     name = os.name
     if name == "ne":
         command = "git branch"
-    elif name == "posix":
-        command = "/usr/bin/git branch"
+
     result = subprocess.run(command, capture_output=True, text=True)
     return result.stdout

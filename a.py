abf1682228ac74f3ae6b44c4d756f3ce8bab26ad
lukos123 <max.kabyshev@gmail.com>
2024-04-27 21:47:14 +0200

    modified: __pycache__/command_type.cpython-311.pyc modified: a.py modified: command.json modified: command_linuks.json modified: command_type.py modified: command_windows.json new file: text

--- a.py
+++ a.py
@@ -1,8 +1,8 @@
 import json
 
 
-command_json = "command_linuks.json"
-command_json_result = "command_linuks.json"
+command_json = "command backap.json"
+command_json_result = "command.json"
 data = {}
 data_result = []
 with open(command_json, 'r') as f:
@@ -13,16 +13,18 @@ def rec(data):
     ready_arr = []
     for i in data:
         if data[i] != "N":
-            if "_c_addon" in i:
+            if "_c_addon" in i or "c_all" in i:
                 continue
             next = []
             next_always = False
             compile_function = ""
+            addon_with_compile_function = []
             if isinstance(data[i], str):
                 next_always = True
                 compile_function = data[i]
                 if f"{i}_c_addon" in data:
                     addon_with_compile_function = rec(data[f"{i}_c_addon"])
+
                     if "c_all" in data[f"{i}_c_addon"]:
                         if isinstance(data[f"{i}_c_addon"]["c_all"], str):
                             next = data[f"{i}_c_addon"]["c_all"]
@@ -31,15 +33,14 @@ def rec(data):
 
             else:
                 next = rec(data[i])
-            addon_with_compile_function = []
 
             ready_arr.append({
-                "command": i.split(" ")[0],
-                "help": " ".join(i.split(" ")[1:]).replace("[", "").replace("]", ""),
-                "compile_function": compile_function,
-                "addon_with_compile_function": addon_with_compile_function,
-                "next_always": next_always,
-                "next": next
+                "c": i.split(" ")[0],
+                "h": " ".join(i.split(" ")[1:]).replace("[", "").replace("]", ""),
+                "cf": compile_function,
+                "awcf": addon_with_compile_function,
+                "na": next_always,
+                "n": next
             })
         else:
             next = []
@@ -49,12 +50,12 @@ def rec(data):
             next_always = False
 
             ready_arr.append({
-                "command": i.split(" ")[0],
-                "help": " ".join(i.split(" ")[1:]).replace("[", "").replace("]", ""),
-                "compile_function": "",
-                "addon_with_compile_function": addon_with_compile_function,
-                "next_always": next_always,
-                "next": next
+                "c": i.split(" ")[0],
+                "h": " ".join(i.split(" ")[1:]).replace("[", "").replace("]", ""),
+                "cf": "",
+                "awcf": addon_with_compile_function,
+                "na": next_always,
+                "n": next
             })
     return ready_arr
 
